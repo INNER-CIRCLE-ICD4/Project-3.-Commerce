87d8a22845420b85b521c0d26228e26c2927a859
sungwoongNoh <asdz453@gmail.com>
2025-07-16 23:51:36 +0900

    chore(msa): spring config server 암호화 데이터 추가 - run 환경변수에 암호화 password 필요

--- infra/config-server/build.gradle.kts
+++ infra/config-server/build.gradle.kts
@@ -2,6 +2,9 @@ val springCloudVersion by extra("2025.0.0")
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
+	implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- infra/config-server/src/main/java/innercircle/configserver/ConfigServerApplication.java
+++ infra/config-server/src/main/java/innercircle/configserver/ConfigServerApplication.java
@@ -1,9 +1,14 @@
 package innercircle.configserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@Slf4j
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
