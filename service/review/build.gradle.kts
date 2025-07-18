fdb96cf0f7d88e57a923a6a219f8d00ceb1cbf05
sungwoongNoh <asdz453@gmail.com>
2025-07-18 23:40:21 +0900

    feature(eureka): Eureka 서버 설정 및 테스트 - eureka-server 설정 - review 모듈에 테스트

--- service/review/build.gradle.kts
+++ service/review/build.gradle.kts
@@ -1,4 +1,11 @@
+val springCloudVersion by extra("2025.0.0")
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation(project(":common:snowflake"))
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
