d04424fbd81e93f0425d5323c122559852b679c5
sungwoongNoh <asdz453@gmail.com>
2025-07-18 22:50:44 +0900

    chore(msa): spring cloud 버전 설정 root에서 공통으로 관리 - config-server 적용 - gateway 적용 - eureka-server 생성 및 적용

--- infra/config-server/src/test/java/innercircle/configserver/config/JasyptConfigTest.java
+++ infra/config-server/src/test/java/innercircle/configserver/config/JasyptConfigTest.java
@@ -8,27 +8,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class JasyptConfigTest {
 
-    @Test
-    void jasyptTest() {
-
-        String value = "변경할 데이터";
-
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        SimplePBEConfig config = new SimplePBEConfig();
-
-        config.setPassword("암호화할 비밀번호");
-        config.setAlgorithm("PBEWithMD5AndDES");
-
-        encryptor.setConfig(config);
-
-        String encryptedText = encryptor.encrypt(value);
-        String decryptedText = encryptor.decrypt(encryptedText);
-
-
-        System.out.println(encryptedText);
-        System.out.println(decryptedText);
-
-        assertThat(encryptedText).isNotEqualTo(value);
-        assertThat(decryptedText).isEqualTo(value);
-    }
+//    @Test
+//    void jasyptTest() {
+//
+//        String value = "변경할 데이터";
+//
+//        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//        SimplePBEConfig config = new SimplePBEConfig();
+//
+//        config.setPassword("암호화할 비밀번호");
+//        config.setAlgorithm("PBEWithMD5AndDES");
+//
+//        encryptor.setConfig(config);
+//
+//        String encryptedText = encryptor.encrypt(value);
+//        String decryptedText = encryptor.decrypt(encryptedText);
+//
+//
+//        System.out.println(encryptedText);
+//        System.out.println(decryptedText);
+//
+//        assertThat(encryptedText).isNotEqualTo(value);
+//        assertThat(decryptedText).isEqualTo(value);
+//    }
 }
