a1c6abdbc5964ca423372c5b44d1182fcec4b6a4
sungwoongNoh <asdz453@gmail.com>
2025-07-26 10:02:25 +0900

    chore(service): member  모듈 cicd 추가 -member 모듈 cicd 추가
    todo
    - 추후 fargate 형태로 배포하려면 ecs 클러스터에서 모듈별 task definition과 service를 생성 해놓아야 함.
    - 클러스터 네트워크 설정 필요, 게이트 웨이만 로드밸런서를 통해 외부 연결

--- common/snowflake/src/main/java/innercircle/commerce/common/snowflake/Snowflake.java
+++ common/snowflake/src/main/java/innercircle/commerce/common/snowflake/Snowflake.java
@@ -3,7 +3,6 @@ package innercircle.commerce.common.snowflake;
 import java.util.random.RandomGenerator;
 
 public class Snowflake {
-
 	private static final int UNUSED_BITS = 1;
 	private static final int EPOCH_BITS = 41;
 	private static final int NODE_ID_BITS = 10;
