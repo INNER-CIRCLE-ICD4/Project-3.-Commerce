d3552e7268bef91951317943c4d2dc3447091c11
sungwoongNoh <asdz453@gmail.com>
2025-07-30 20:23:46 +0900

    test(member): Member 회원가입 컨트롤러 테스트 - Member 생성 요청 테스트

---
+++ service/member/src/test/java/innercircle/member/infrastructure/adapter/in/MemberControllerTest.java
@@ -0,0 +1,67 @@
+package innercircle.member.infrastructure.adapter.in;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import innercircle.commerce.common.snowflake.Snowflake;
+import innercircle.member.application.MemberCreateRequest;
+import innercircle.member.application.MemberResponse;
+import innercircle.member.application.port.in.MemberUseCase;
+import innercircle.member.domain.Gender;
+import innercircle.member.domain.MemberRole;
+import innercircle.member.domain.MemberStatus;
+import innercircle.member.domain.RoleType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+@AutoConfigureMockMvc
+class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberUseCase memberUseCase;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    @DisplayName("회원가입 요청 - 성공")
+    void memberCreate() throws Exception {
+
+        MemberCreateRequest memberCreateRequest = new MemberCreateRequest("swnoh@google.com", "노성웅", "12345678A", "1996-04-23", "MAIL");
+        MemberResponse response = new MemberResponse(new Snowflake().nextId(), memberCreateRequest.email(), memberCreateRequest.name(), LocalDate.of(1996, Month.APRIL, 23), Gender.MAIL.name(), MemberStatus.ACTIVE, LocalDateTime.now(), List.of(RoleType.BUYER.name()));
+
+        when(memberUseCase.createMember(memberCreateRequest))
+                .thenReturn(response);
+
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.memberId").value(response.memberId()));
+
+        verify(memberUseCase, times(1)).createMember(any(MemberCreateRequest.class));
+        verifyNoMoreInteractions(memberUseCase);
+    }
+}
