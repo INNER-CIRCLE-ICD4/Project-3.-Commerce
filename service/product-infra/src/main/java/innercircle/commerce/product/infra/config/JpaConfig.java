4fd9d0b8b8cf8d386ed6250c102e02fe92555a77
HwangInUng <ung6860@naver.com>
2025-08-25 19:22:36 +0900

    docs: index.html 추가

--- service/product-infra/src/main/java/innercircle/commerce/product/infra/config/JpaConfig.java
+++ service/product-infra/src/main/java/innercircle/commerce/product/infra/config/JpaConfig.java
@@ -3,12 +3,14 @@ package innercircle.commerce.product.infra.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * JPA 설정
  */
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "innercircle.commerce.product.infra.repository")
 @EntityScan(basePackages = "innercircle.commerce.product.infra.entity")
 public class JpaConfig {
 }
